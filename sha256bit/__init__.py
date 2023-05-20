764dee1ad395f1ba79c0a5f8a6482a3bbf66ddc7
sebastien-riou <sriou@nimp.co.uk>
2023-05-20 14:27:16 -0700

    reformat using black

--- sha256bit/__init__.py
+++ sha256bit/__init__.py
@@ -4,30 +4,85 @@ import binascii
 
 
 class sha256bit(object):
-
     F32 = 0xFFFFFFFF
 
-    _k = [  
-        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
-        0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-        0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
-        0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-        0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
-        0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-        0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
-        0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-        0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
-        0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-        0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
-        0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-        0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
-        0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-        0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
-        0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2]
+    _k = [
+        0x428A2F98,
+        0x71374491,
+        0xB5C0FBCF,
+        0xE9B5DBA5,
+        0x3956C25B,
+        0x59F111F1,
+        0x923F82A4,
+        0xAB1C5ED5,
+        0xD807AA98,
+        0x12835B01,
+        0x243185BE,
+        0x550C7DC3,
+        0x72BE5D74,
+        0x80DEB1FE,
+        0x9BDC06A7,
+        0xC19BF174,
+        0xE49B69C1,
+        0xEFBE4786,
+        0x0FC19DC6,
+        0x240CA1CC,
+        0x2DE92C6F,
+        0x4A7484AA,
+        0x5CB0A9DC,
+        0x76F988DA,
+        0x983E5152,
+        0xA831C66D,
+        0xB00327C8,
+        0xBF597FC7,
+        0xC6E00BF3,
+        0xD5A79147,
+        0x06CA6351,
+        0x14292967,
+        0x27B70A85,
+        0x2E1B2138,
+        0x4D2C6DFC,
+        0x53380D13,
+        0x650A7354,
+        0x766A0ABB,
+        0x81C2C92E,
+        0x92722C85,
+        0xA2BFE8A1,
+        0xA81A664B,
+        0xC24B8B70,
+        0xC76C51A3,
+        0xD192E819,
+        0xD6990624,
+        0xF40E3585,
+        0x106AA070,
+        0x19A4C116,
+        0x1E376C08,
+        0x2748774C,
+        0x34B0BCB5,
+        0x391C0CB3,
+        0x4ED8AA4A,
+        0x5B9CCA4F,
+        0x682E6FF3,
+        0x748F82EE,
+        0x78A5636F,
+        0x84C87814,
+        0x8CC70208,
+        0x90BEFFFA,
+        0xA4506CEB,
+        0xBEF9A3F7,
+        0xC67178F2,
+    ]
 
-    _h_init = [ 
-        0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
-        0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19]
+    _h_init = [
+        0x6A09E667,
+        0xBB67AE85,
+        0x3C6EF372,
+        0xA54FF53A,
+        0x510E527F,
+        0x9B05688C,
+        0x1F83D9AB,
+        0x5BE0CD19,
+    ]
 
     @staticmethod
     def _rotr(x, y):
@@ -47,13 +102,13 @@ class sha256bit(object):
     digest_size = 32
 
     def __init__(self, m=None, *, bitlen=None):
-        """ SHA-256 implementation supporting bit granularity for message input length.
-            API is the same as hashlib.
+        """SHA-256 implementation supporting bit granularity for message input length.
+        API is the same as hashlib.
         """
         self._counter = 0
         self._cache = bytearray()
         self._h = copy.deepcopy(sha256bit._h_init)
-        self._has_bitlen = False 
+        self._has_bitlen = False
         self._digest = None
         self.update(m, bitlen=bitlen)
 
@@ -68,7 +123,7 @@ class sha256bit(object):
 
     @staticmethod
     def import_state(state):
-        o=sha256bit()
+        o = sha256bit()
         o._counter = state["cnt"]
         if 0 != (o._counter % 8):
             o._has_bitlen = True
@@ -83,15 +138,19 @@ class sha256bit(object):
 
     def _compress(self, c):
         w = [0] * 64
-        w[0:16] = struct.unpack('!16L', c)
+        w[0:16] = struct.unpack("!16L", c)
         for i in range(16, 64):
-            s0 = ( sha256bit._rotr(w[i-15], 7) ^ 
-                   sha256bit._rotr(w[i-15], 18) ^ 
-                   (w[i-15] >> 3))
-            s1 = ( sha256bit._rotr(w[i-2], 17) ^ 
-                   sha256bit._rotr(w[i-2], 19) ^ 
-                   (w[i-2] >> 10))
-            w[i] = (w[i-16] + s0 + w[i-7] + s1) & sha256bit.F32
+            s0 = (
+                sha256bit._rotr(w[i - 15], 7)
+                ^ sha256bit._rotr(w[i - 15], 18)
+                ^ (w[i - 15] >> 3)
+            )
+            s1 = (
+                sha256bit._rotr(w[i - 2], 17)
+                ^ sha256bit._rotr(w[i - 2], 19)
+                ^ (w[i - 2] >> 10)
+            )
+            w[i] = (w[i - 16] + s0 + w[i - 7] + s1) & sha256bit.F32
 
         a, b, c, d, e, f, g, h = self._h
 
@@ -114,9 +173,9 @@ class sha256bit(object):
             self._h[i] = (x + y) & sha256bit.F32
 
     def update(self, m, *, bitlen=None):
-        """ Update the hash object with the bytes in data. Repeated calls
-            are equivalent to a single call with the concatenation of all
-            the arguments.
+        """Update the hash object with the bytes in data. Repeated calls
+        are equivalent to a single call with the concatenation of all
+        the arguments.
         """
         if not m:
             return
@@ -125,8 +184,10 @@ class sha256bit(object):
             if 0 != (bitlen % 8):
                 self._has_bitlen = True
             else:
-                assert bitlen == len(m) * 8, \
-                "bitLen=%d, len(m)*8=%d"%(bitlen, len(m) * 8)
+                assert bitlen == len(m) * 8, "bitLen=%d, len(m)*8=%d" % (
+                    bitlen,
+                    len(m) * 8,
+                )
             self._counter += bitlen
         else:
             self._counter += len(m) * 8
@@ -147,46 +208,46 @@ class sha256bit(object):
 
     def _pad(self):
         lastBlockBitLen = self._counter % 512
-        lastBlockFullBytesCnt = lastBlockBitLen//8
+        lastBlockFullBytesCnt = lastBlockBitLen // 8
         if lastBlockBitLen < 448:
             padlen = 55 - lastBlockFullBytesCnt
         else:
             padlen = 119 - lastBlockFullBytesCnt
         if False:
-            print(lastBlockBitLen,lastBlockFullBytesCnt,padlen, len(self._cache))
+            print(lastBlockBitLen, lastBlockFullBytesCnt, padlen, len(self._cache))
 
         shift = self._counter % 8
         if shift > 0 and (len(self._cache) > 0):
-            mask = 0xFF << (8-shift)
+            mask = 0xFF << (8 - shift)
             self._cache[-1] = (self._cache[-1] & mask) | (0x80 >> shift)
         else:
-            self._cache += b'\x80'
-        self._cache += (b'\x00' * padlen) + self._counter.to_bytes(8, byteorder='big')
+            self._cache += b"\x80"
+        self._cache += (b"\x00" * padlen) + self._counter.to_bytes(8, byteorder="big")
         if False:
             from pysatl import Utils
-            print("counter=%d (0x%x)"%(self._counter,self._counter))
+
+            print("counter=%d (0x%x)" % (self._counter, self._counter))
             print(Utils.hexstr(self._cache))
-        assert len(self._cache) in [64,128], "len(self._cache)=%d"%len(self._cache)
-    
+        assert len(self._cache) in [64, 128], "len(self._cache)=%d" % len(self._cache)
+
     def digest(self):
-        """ Return the digest of the bytes passed to the update() method
-            so far as a bytes object.
+        """Return the digest of the bytes passed to the update() method
+        so far as a bytes object.
         """
         if self._digest is not None:
             return self._digest
         self._pad()
-        blocks = [self._cache[i:i + 64] for i in range(0, len(self._cache), 64)]
+        blocks = [self._cache[i : i + 64] for i in range(0, len(self._cache), 64)]
         for b in blocks:
             self._compress(b)
-        data = [struct.pack('!L', i) for i in self._h[:self._output_size]]
-        self._digest = b''.join(data)
+        data = [struct.pack("!L", i) for i in self._h[: self._output_size]]
+        self._digest = b"".join(data)
         self._cache = None
         self._h = None
         return self._digest
 
     def hexdigest(self):
-        """ Like digest() except the digest is returned as a string
-            of double length, containing only hexadecimal digits.
+        """Like digest() except the digest is returned as a string
+        of double length, containing only hexadecimal digits.
         """
-        return binascii.hexlify(self.digest()).decode('ascii')
-    
+        return binascii.hexlify(self.digest()).decode("ascii")
--- test/test.py
+++ test/test.py
@@ -4,13 +4,13 @@ import re
 from sha256bit import sha256bit
 
 
-def block_generator(seed,msg_bitlen):
+def block_generator(seed, msg_bitlen):
     l2block = hashlib.sha256(seed).digest()
     l2block += hashlib.sha256(l2block).digest()
-    blockBitLen = len(l2block)*8
-    assert blockBitLen == 64*8
-    bitlen=0
-    while(bitlen+blockBitLen<msg_bitlen):
+    blockBitLen = len(l2block) * 8
+    assert blockBitLen == 64 * 8
+    bitlen = 0
+    while bitlen + blockBitLen < msg_bitlen:
         yield l2block
         bitlen += blockBitLen
         l2block = hashlib.sha256(l2block).digest()
@@ -18,73 +18,94 @@ def block_generator(seed,msg_bitlen):
     # last block
     lastBlockBitLen = msg_bitlen % 512
     if 0 != lastBlockBitLen:
-        lastBlockFullByteLen = (lastBlockBitLen+7)//8
+        lastBlockFullByteLen = (lastBlockBitLen + 7) // 8
         l2block = bytearray(l2block[0:lastBlockFullByteLen])
         assert len(l2block) == lastBlockFullByteLen
-        mask = 0xFF & (0xFF << (8-(msg_bitlen % 8)))
+        mask = 0xFF & (0xFF << (8 - (msg_bitlen % 8)))
         if 0 != mask:
-            l2block[-1] &= mask 
+            l2block[-1] &= mask
     yield l2block
+
 
 def msg_generator(seed, msg_bitlen):
     o = bytearray()
-    for b in block_generator(seed,msg_bitlen):
-        o += b 
+    for b in block_generator(seed, msg_bitlen):
+        o += b
     return o
 
-def check_against_hashlib(n_seeds=3,max_length=1024*4):
+
+def check_against_hashlib(n_seeds=3, max_length=1024 * 4):
     print("check against hashlib")
 
     assert hashlib.sha256("abc".encode()).digest() == sha256bit("abc".encode()).digest()
 
-    def check_against_hashlib(seed,msg_bitlen):
+    def check_against_hashlib(seed, msg_bitlen):
         expected = hashlib.sha256()
         dut = sha256bit()
-        for block in block_generator(seed,msg_bitlen):
-            #print(Utils.hexstr(block))
+        for block in block_generator(seed, msg_bitlen):
+            # print(Utils.hexstr(block))
             expected.update(block)
             dut.update(block)
         assert expected.digest() == dut.digest()
 
     for seedByte in range(0, n_seeds):
-        for msgBitLen in range(0,max_length, 8):
+        for msgBitLen in range(0, max_length, 8):
             seed = bytearray([seedByte])
-            check_against_hashlib(seed,msgBitLen)
+            check_against_hashlib(seed, msgBitLen)
+
 
 def check(msg, bitlen, sig):
-        m = sha256bit()
-        if isinstance(msg,str):
-            msg=msg.encode('ascii')
-        descr = "msg      = "+Utils.hexstr(msg)+"\n"
-        descr+= "bitlen   = %d\n"%bitlen
-        descr+= "expected = "+sig+"\n"
-        try:
-            
-            m.update(msg, bitlen=bitlen)
-            digest = m.hexdigest()
-        except Exception as e:
-            print(descr)
-            raise e
-        errMsg ='\n'
-        errMsg+= descr
-        errMsg+= "digest   = "+digest+"\n"
-        assert digest == sig, errMsg
+    m = sha256bit()
+    if isinstance(msg, str):
+        msg = msg.encode("ascii")
+    descr = "msg      = " + Utils.hexstr(msg) + "\n"
+    descr += "bitlen   = %d\n" % bitlen
+    descr += "expected = " + sig + "\n"
+    try:
+        m.update(msg, bitlen=bitlen)
+        digest = m.hexdigest()
+    except Exception as e:
+        print(descr)
+        raise e
+    errMsg = "\n"
+    errMsg += descr
+    errMsg += "digest   = " + digest + "\n"
+    assert digest == sig, errMsg
+
 
 def check_hardcoded_test_vectors():
     print("check few minimal hardcoded test vectors")
-    
+
     tests = [
-        {"msg":"","bitlen":0,"digest":'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'},
-        {"msg":"a","bitlen":8,"digest":'ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb'},
-        {"msg":Utils.ba("00"),"bitlen":1,"digest":'bd4f9e98beb68c6ead3243b1b4c7fed75fa4feaab1f84795cbd8a98676a2a375'},
-        {"msg":Utils.ba("80"),"bitlen":2,"digest":'18f331f626210ff9bad6995d8cff6e891adba50eb2fdbddcaa921221cdc333ae'},
+        {
+            "msg": "",
+            "bitlen": 0,
+            "digest": "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+        },
+        {
+            "msg": "a",
+            "bitlen": 8,
+            "digest": "ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb",
+        },
+        {
+            "msg": Utils.ba("00"),
+            "bitlen": 1,
+            "digest": "bd4f9e98beb68c6ead3243b1b4c7fed75fa4feaab1f84795cbd8a98676a2a375",
+        },
+        {
+            "msg": Utils.ba("80"),
+            "bitlen": 2,
+            "digest": "18f331f626210ff9bad6995d8cff6e891adba50eb2fdbddcaa921221cdc333ae",
+        },
     ]
 
     for test in tests:
-        check(test["msg"],test["bitlen"],test["digest"])
+        check(test["msg"], test["bitlen"], test["digest"])
 
-    assert sha256bit(b'\x00',bitlen=1).hexdigest() == \
-        'bd4f9e98beb68c6ead3243b1b4c7fed75fa4feaab1f84795cbd8a98676a2a375'
+    assert (
+        sha256bit(b"\x00", bitlen=1).hexdigest()
+        == "bd4f9e98beb68c6ead3243b1b4c7fed75fa4feaab1f84795cbd8a98676a2a375"
+    )
 
 
 def check_against_nist_cavp():
@@ -92,8 +113,9 @@ def check_against_nist_cavp():
     # (https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Algorithm-Validation-Program/documents/shs/shabittestvectors.zip)
 
     from pathlib import Path
-    resource_path = Path(__file__).parent 
-    for tvFile in ["SHA256ShortMsg.rsp","SHA256LongMsg.rsp"]:
+
+    resource_path = Path(__file__).parent
+    for tvFile in ["SHA256ShortMsg.rsp", "SHA256LongMsg.rsp"]:
         tvPath = resource_path.joinpath(tvFile)
         with open(tvPath) as f:
             for line in f:
@@ -107,27 +129,28 @@ def check_against_nist_cavp():
                     MD = re.search(r"MD = (.+)", line).group(1)
                     check(msg, bitlen, MD)
 
+
 def check_api():
     print("check API")
-    msg = msg_generator(bytes(0),300*8)
+    msg = msg_generator(bytes(0), 300 * 8)
     expected = hashlib.sha256(msg).digest()
-    #print(Utils.hexstr(msg))
-    #print(Utils.hexstr(expected))
+    # print(Utils.hexstr(msg))
+    # print(Utils.hexstr(expected))
     assert expected == sha256bit(msg).digest()
-    for len1 in range(0,len(msg)*8):
+    for len1 in range(0, len(msg) * 8):
         dut1 = sha256bit()
         dut1.update(msg[:len1])
         state = dut1.export_state()
         dut2 = sha256bit.import_state(state)
         dut2.update(msg[len1:])
         assert expected == dut2.digest()
-    for len1 in range(1,len(msg)*8):
+    for len1 in range(1, len(msg) * 8):
         dut = sha256bit()
         remaining = len(msg)
         p = 0
-        while remaining>0:
-            chunk = msg[p:p+len1]
-            dut.update(chunk, bitlen=len(chunk)*8)
+        while remaining > 0:
+            chunk = msg[p : p + len1]
+            dut.update(chunk, bitlen=len(chunk) * 8)
             p += len1
             remaining -= len1
         assert expected == dut.digest()
@@ -135,12 +158,10 @@ def check_api():
         dut2 = sha256bit.import_state(state)
         assert expected == dut2.digest()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     check_api()
     check_hardcoded_test_vectors()
     check_against_nist_cavp()
-    check_against_hashlib(n_seeds=3,max_length=1024*4)
+    check_against_hashlib(n_seeds=3, max_length=1024 * 4)
     print("All test PASS")
-                
-            
-    
