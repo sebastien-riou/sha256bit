c8a59f9c623187a34dc086a0efa1e01e81bd5e36
sebastien-riou <sriou@nimp.co.uk>
2023-05-21 02:58:01 -0700

    add sphinx doc skeleton

---
+++ docs/source/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'sha256bit'
+copyright = '2023, Sebastien Riou'
+author = 'Sebastien Riou'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+
+extensions = [
+    'sphinx_rtd_theme',
+    'sphinx_multiversion',
+]
+
+smv_tag_whitelist = r'^v\d+\.\d+.*$|latest'  # all tags of form v*.*.x and latest
+# Whitelist pattern for branches (set to '' to ignore all branches)
+smv_branch_whitelist = ''
+smv_released_pattern = r'v.*'
+smv_latest_version = 'v0.0'
+smv_remote_whitelist = None
+
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. sha256bit documentation master file, created by
+   sphinx-quickstart on Sun May 21 02:49:23 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to sha256bit's documentation!
+=====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
