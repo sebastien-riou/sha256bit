428964ad86e2314c16fbe924db8309eb5eb48be6
sebastien-riou <sriou@nimp.co.uk>
2023-06-20 20:14:31 -0700

    black lint

--- sha256bit/__init__.py
+++ sha256bit/__init__.py
@@ -3,10 +3,13 @@ import copy
 import struct
 import sys
 import logging
+
 try:
     from pysatl import Utils
 except ImportError:
-    pass # we just don't support logging
+    pass  # we just don't support logging
+
+
 class Sha256bit:
     F32 = 0xFFFFFFFF
 
@@ -126,15 +129,15 @@ class Sha256bit:
             c = self._cache
             if self._verbose:
                 logging.info("exporting current state:")
-                logging.info("  bitlen = %d"%self._counter)
-                logging.info("  state:  "+Utils.hexstr(self._state_bytes()))
-                logging.info("  cache:  "+Utils.hexstr(c))
+                logging.info("  bitlen = %d" % self._counter)
+                logging.info("  state:  " + Utils.hexstr(self._state_bytes()))
+                logging.info("  cache:  " + Utils.hexstr(c))
         else:
             h = self._digest
             c = None
             if self._verbose:
                 logging.info("exporting finalized digest:")
-                logging.info("  digest:  "+Utils.hexstr(h))
+                logging.info("  digest:  " + Utils.hexstr(h))
         return {'h': h, 'cnt': self._counter, 'cache': c}
 
     @staticmethod
@@ -151,15 +154,15 @@ class Sha256bit:
             o._cache = None
             if o._verbose:
                 logging.info("importing finalized digest:")
-                logging.info("  digest:  "+Utils.hexstr(o._digest))
+                logging.info("  digest:  " + Utils.hexstr(o._digest))
         else:
             o._h = state['h']
             o._cache = bytearray(state['cache'])
             if o._verbose:
                 logging.info("importing current state:")
-                logging.info("  bitlen = %d"%o._counter)
-                logging.info("  state:  "+Utils.hexstr(o._state_bytes()))
-                logging.info("  cache:  "+Utils.hexstr(o._cache))
+                logging.info("  bitlen = %d" % o._counter)
+                logging.info("  state:  " + Utils.hexstr(o._state_bytes()))
+                logging.info("  cache:  " + Utils.hexstr(o._cache))
         return o
 
     def _state_bytes(self):
@@ -167,8 +170,8 @@ class Sha256bit:
 
     def _compress(self, block):
         if self._verbose:
-            logging.info("state:  "+Utils.hexstr(self._state_bytes()))
-            logging.info("block:  "+Utils.hexstr(block))
+            logging.info("state:  " + Utils.hexstr(self._state_bytes()))
+            logging.info("block:  " + Utils.hexstr(block))
 
         w = [0] * 64
         w[0:16] = struct.unpack('!16L', block)
@@ -182,14 +185,14 @@ class Sha256bit:
 
         if self._verbose:
             logging.debug("current state:")
-            logging.debug("  a = 0x%08x"%(a))
-            logging.debug("  b = 0x%08x"%(b))
-            logging.debug("  c = 0x%08x"%(c))
-            logging.debug("  d = 0x%08x"%(d))
-            logging.debug("  e = 0x%08x"%(e))
-            logging.debug("  f = 0x%08x"%(f))
-            logging.debug("  g = 0x%08x"%(g))
-            logging.debug("  h = 0x%08x"%(h))
+            logging.debug("  a = 0x%08x" % (a))
+            logging.debug("  b = 0x%08x" % (b))
+            logging.debug("  c = 0x%08x" % (c))
+            logging.debug("  d = 0x%08x" % (d))
+            logging.debug("  e = 0x%08x" % (e))
+            logging.debug("  f = 0x%08x" % (f))
+            logging.debug("  g = 0x%08x" % (g))
+            logging.debug("  h = 0x%08x" % (h))
 
         for i in range(64):
             s0 = Sha256bit._rotr(a, 2) ^ Sha256bit._rotr(a, 13) ^ Sha256bit._rotr(a, 22)
@@ -207,21 +210,21 @@ class Sha256bit:
             a = (t1 + t2) & Sha256bit.F32
 
             if self._verbose:
-                logging.debug("state after round %d"%(i+1))
-                logging.debug("  k[%02d] = 0x%08x"%(i,Sha256bit._k[i]))
-                logging.debug("  w[%02d] = 0x%08x"%(i,w[i]))
-                logging.debug("  s0    = 0x%08x"%(s0))
-                logging.debug("  s1    = 0x%08x"%(s1))
-                logging.debug("  t1    = 0x%08x"%(t1))
-                logging.debug("  t2    = 0x%08x"%(t2))
-                logging.debug("  a     = 0x%08x"%(a))
-                logging.debug("  b     = 0x%08x"%(b))
-                logging.debug("  c     = 0x%08x"%(c))
-                logging.debug("  d     = 0x%08x"%(d))
-                logging.debug("  e     = 0x%08x"%(e))
-                logging.debug("  f     = 0x%08x"%(f))
-                logging.debug("  g     = 0x%08x"%(g))
-                logging.debug("  h     = 0x%08x"%(h))
+                logging.debug("state after round %d" % (i + 1))
+                logging.debug("  k[%02d] = 0x%08x" % (i, Sha256bit._k[i]))
+                logging.debug("  w[%02d] = 0x%08x" % (i, w[i]))
+                logging.debug("  s0    = 0x%08x" % (s0))
+                logging.debug("  s1    = 0x%08x" % (s1))
+                logging.debug("  t1    = 0x%08x" % (t1))
+                logging.debug("  t2    = 0x%08x" % (t2))
+                logging.debug("  a     = 0x%08x" % (a))
+                logging.debug("  b     = 0x%08x" % (b))
+                logging.debug("  c     = 0x%08x" % (c))
+                logging.debug("  d     = 0x%08x" % (d))
+                logging.debug("  e     = 0x%08x" % (e))
+                logging.debug("  f     = 0x%08x" % (f))
+                logging.debug("  g     = 0x%08x" % (g))
+                logging.debug("  h     = 0x%08x" % (h))
 
         for i, (x, y) in enumerate(zip(self._h, [a, b, c, d, e, f, g, h])):
             self._h[i] = (x + y) & Sha256bit.F32
@@ -277,15 +280,15 @@ class Sha256bit:
             padlen = 55 - last_block_full_bytes_cnt
         else:
             padlen = 119 - last_block_full_bytes_cnt
-        
+
         shift = self._counter % 8
-        
+
         if self._verbose:
-            logging.debug("bitlen = %d"%self._counter)
-            logging.debug("last_block_bitlen = %d"%last_block_bitlen)
-            logging.debug("last_block_full_bytes_cnt = %d"%last_block_full_bytes_cnt)
-            logging.debug("padlen = %d"%padlen)
-            logging.debug("shift = %d"%shift)
+            logging.debug("bitlen = %d" % self._counter)
+            logging.debug("last_block_bitlen = %d" % last_block_bitlen)
+            logging.debug("last_block_full_bytes_cnt = %d" % last_block_full_bytes_cnt)
+            logging.debug("padlen = %d" % padlen)
+            logging.debug("shift = %d" % shift)
 
         if shift > 0 and (len(self._cache) > 0):
             mask = 0xFF << (8 - shift)
@@ -305,8 +308,8 @@ class Sha256bit:
         if self._digest is not None:
             return self._digest
         if self._verbose:
-            logging.info ("bitlen: %d"%self._counter)
-        
+            logging.info("bitlen: %d" % self._counter)
+
         self._pad()
         blocks = [self._cache[i : i + 64] for i in range(0, len(self._cache), 64)]
         for b in blocks:
@@ -314,13 +317,12 @@ class Sha256bit:
         data = [struct.pack('!L', i) for i in self._h[: self._output_size]]
         self._digest = b''.join(data)
         if self._verbose:
-            logging.debug("state:  "+Utils.hexstr(self._state_bytes()))
-            logging.info ("digest: "+Utils.hexstr(self._digest))
-        
+            logging.debug("state:  " + Utils.hexstr(self._state_bytes()))
+            logging.info("digest: " + Utils.hexstr(self._digest))
 
         self._cache = None
         self._h = None
-    
+
         return self._digest
 
     def hexdigest(self):
@@ -328,4 +330,3 @@ class Sha256bit:
         of double length, containing only hexadecimal digits.
         """
         return binascii.hexlify(self.digest()).decode('ascii')
-
--- sha256bit/cli.py
+++ sha256bit/cli.py
@@ -5,14 +5,13 @@ import argparse
 import logging
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(description='sha256bit.cli')
     levels = ('DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL')
     parser.add_argument('--log-level', default='WARNING', choices=levels)
-    parser.add_argument('--bit-length', help='Bit length of message',default=None, type=int)
-    parser.add_argument('message', nargs=1, help='Message to hash',type=str)
+    parser.add_argument('--bit-length', help='Bit length of message', default=None, type=int)
+    parser.add_argument('message', nargs=1, help='Message to hash', type=str)
     args = parser.parse_args()
     logging.basicConfig(format='%(message)s', level=args.log_level)
     msg = Utils.ba(args.message[0])
-    digest  = Sha256bit(msg,bitlen=args.bit_length).digest()
+    digest = Sha256bit(msg, bitlen=args.bit_length).digest()
     print(Utils.hexstr(digest))
